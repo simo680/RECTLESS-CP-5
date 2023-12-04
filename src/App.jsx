fff77ba630ddab685bec3d4dbf15dfda2c03f8df
simo680 <erik.simonyan2005@gmail.com>
2023-12-04 19:40:15 +0300

    create ProductList and store for redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ProductList from './components/ProductList/ProductList';
+
+
+function App() {
+  return (
+    <div className="App"> 
+    <ProductList/>
+    </div>
+  );
+}
+
+export default App;
